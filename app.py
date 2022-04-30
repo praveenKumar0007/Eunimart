fd55a5f8881e0570079240c44148723c00dddb18
Praveen Kumar S <84445957+praveenKumar0007@users.noreply.github.com>
2022-04-30 13:06:18 +0530

    Customer Verification Added

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask_restful import Api, Resource, reqparse, abort
 
 import os
 from dotenv import load_dotenv
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 my_number = os.environ.get("my_number")
 account_sid = os.environ.get("account_sid")
@@ -14,6 +13,7 @@ import twilio
 from twilio.rest import Client
 import random
 
+customers = {}
 app = Flask(__name__)
 api = Api(app) # Initializes Restful Api
 
@@ -27,20 +27,37 @@ class customer_login(Resource): # customer_login class will Inherit from Resourc
         return {"welcome_text" : "Welcome to Bank"} # Returned object should be serializable( can be converted to JSON format like Javascript objects)
     def put(self):
         args = customer_login_put_arguments.parse_args()
+        random_customer_id = random.randint(10000,99999)
         otp = random.randint(1000,9999)
-        print("Your OTP is - ",otp)
- 
         client = Client(account_sid, auth_token)
-
         message = client.messages.create(
-                body='OTP is - ' + str(otp),
+                body='Hello ' + args.customer_first_name + ' ' + args.customer_last_name + '\n' + 'Your OTP is ' + str(otp) +'\n' + 'Your Customer ID is ' + str(random_customer_id),
                 from_=my_number,
                 to=args.mobile_number_with_ctry_code
             )
-
-        print(message.sid)
-        return {"Login": args}
+        customers[random_customer_id] = args
+        customers[random_customer_id]["otp"] = otp
+        customers[random_customer_id]["customer_id"] = random_customer_id
+        print(customers)
+        return {"Login": args,"customer_id":random_customer_id, "status":200}
 api.add_resource(customer_login,"/customer_login") # Register customer_login as a resource.. the "/" is the default/home URL
+
+customer_verification_put_arguments = reqparse.RequestParser()
+customer_verification_put_arguments.add_argument("customer_id", type=int, help="Customer ID is Mandatory", required=True) 
+customer_verification_put_arguments.add_argument("customer_otp", type=int, help="OTP is Mandatory", required=True) 
+
+class customer_verification(Resource):
+    def put(self):
+        args = customer_verification_put_arguments.parse_args()
+        if args.customer_id in customers:
+            if customers[args.customer_id]["otp"] == args.customer_otp:
+                return {"data" : "You are verified now: " + customers[args.customer_id].customer_first_name + customers[args.customer_id].customer_last_name + "\n Welcome to Our Bank"}
+            else:
+                return {"data" : "Invalid OTP","status":401}
+        else:
+            return {"data" : "Invalid Customer ID","status":404}
+        
+api.add_resource(customer_verification,"/customer_verification")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ test_login.py
@@ -0,0 +1,6 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+response = requests.put(BASE + "customer_login", {"customer_first_name":"Praveen","customer_last_name":"Kumar", "mobile_number_with_ctry_code":"+91XXXXXXXXXX"})
+print(response.json()) # To convert the response object ot JSON format
