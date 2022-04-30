df5eb221510a890a0898f5d7f648563bf8a7f55b
Praveen Kumar S <84445957+praveenKumar0007@users.noreply.github.com>
2022-04-30 10:37:36 +0530

    Flask Rest Basic

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app) # Initializes Restful Api
 
-@app.route('/')
-def index():
-    return "Hello Loganand"
+class customer_login(Resource): # customer_login class will Inherit from Resource
+    def get(self):
+        return {"data" : "Hello World"} # Returned object should be serializable( can be converted to JSON format like Javascript objects)
+
+api.add_resource(customer_login,"/") # Register customer_login as a resource.. the "/" is the default/home URL
 
 if __name__ == "__main__":
     app.run(debug=True)
